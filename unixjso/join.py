3212941bc61620c7bb8f86c27a43abb9fa1c4bfe
grochmal <michal.jan.grochmal@gmail.com>
2014-07-18 17:59:19 +0100

    start code for join operation

---
+++ unixjso/join.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import sys,json,fileinput
+from unixjso.core import eprint
+
+def all_lines(args, params, linef, silent=False, pipe=True):
+    input = fileinput.input(args)
+    if pipe:  # correct behaviour over pipes, i.e. finish execution on SIGPIPE
+        import signal
+        signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+    while True:
+        try:
+            line = input.next().strip()
+            if '' == line: continue
+            try:
+                info = { 'file' : fileinput.filename()
+                       , 'line' : fileinput.filelineno()
+                       }
+                js = json.loads(line)
+                if not type(js) is dict:
+                    eprint( 'Non dictionary at file:', info['file']
+                          , 'line:', info['line'], silent=silent
+                          )
+                    continue
+                ret = linef(js, params)
+                if ret: yield ret
+            except ValueError as e: eprint( str(e)
+                                          , 'file:', info['file']
+                                          , 'line:', info['line']
+                                          , silent=silent
+                                          )
+        except IOError as e: eprint(str(e), silent=silent)
+        except StopIteration: break
+
